08259e4bddd5034c825bee35ae49e68c6ea8141f
gohun <lljjll1235@gmail.com>
2022-10-18 21:50:28 +0900

    10.18

---
+++ js/109.banner_slide.js
@@ -0,0 +1,125 @@
+$(document).ready(function(){
+    let index = 0;
+    //배너이미지 삽입
+    for (let i = 0; i < $(".banner").length; i++)
+    {
+        $(".banner").eq(i).css({
+            background: `url(./img/theComma/main_banner/main_img_0${i + 1}.jpg) center / cover`
+        });
+    }
+    //
+    //인디케이터 삽입
+    let b_length = $(".banner").length;
+    for (let i = 0; i < b_length; i++)
+    {
+        $(".indicator").append(`<div class="i_bar"></div>`);
+    }
+    //
+    //인디케이터 채우는 함수
+    function fillIndicator(){
+        $(".i_bar").removeClass("filled");
+        $(".i_bar").eq(index % 3).addClass("filled");
+    }
+    fillIndicator();
+    //
+    //인디케이터 누르면 슬라이드 실행하는 for문
+    $(".i_bar").click(function(){
+        console.log($(this).index());
+        console.log($(".filled").index());
+        if ($(this).index() == $(".filled").index() + 2)
+        {
+            slide(1, 200);
+            fillIndicator();
+            setTimeout(function(){
+                slide(1, 200);
+            }, 200);
+            setTimeout(function(){
+                fillIndicator();
+            }, 300);
+        }
+        else if ($(this).index() == $(".filled").index() + 1)
+        {
+            slide(1, 200);
+            fillIndicator();
+        }
+        else if ($(this).index() == $(".filled").index() - 1)
+        {
+            slide(-1, 200);
+            fillIndicator();
+        }
+        else if ($(this).index() == $(".filled").index() - 2)
+        {
+            slide(-1, 200);
+            fillIndicator();
+            setTimeout(function(){
+                slide(-1, 200);
+            }, 200);
+            setTimeout(function(){
+                fillIndicator();
+            }, 300);
+        }
+    })
+    //
+    //배너 초기위치
+    $(".banner").eq(0).css({
+        left: 0
+    });
+    //
+    //버튼에 함수 탑재
+    $("#btn_r").click(function(){
+        blockBtn();
+        slide(1, 500);
+        fillIndicator()
+    })
+    $("#btn_l").click(function(){
+        blockBtn();
+        slide(-1);
+        fillIndicator()
+    })
+    //
+    //인터벌, 인터벌함수
+    let itv;
+    function autoSlide()
+    {
+        clearInterval(itv);
+        itv = setInterval(function(){
+            slide(1, 500);
+            fillIndicator();
+        }, 2000);
+    }
+    autoSlide();
+    $(".main_banner").mouseenter(function(){
+        clearInterval(itv);
+    })
+    $(".main_banner").mouseleave(function(){
+        clearInterval(itv);
+        autoSlide();
+    })
+    //
+    //슬라이드 함수
+    function slide(pm, timer){
+        $(".banner").eq(index % 3).animate({
+            left: pm * -100 + "%"
+        }, timer);
+        $(".banner").eq((index + pm) % 3).animate({
+            left: pm * 100 + "%"
+        }, 0).animate({
+            left: 0
+        }, timer);
+        index += pm;
+    }
+    //
+    //움직일때 클릭 방지
+    function blockBtn()
+    {
+        $(".btn").css({
+            pointerEvents: "none"
+        });
+        setTimeout(() => {
+            $(".btn").css({
+            pointerEvents: "auto"
+        });
+        }, 500)
+    }
+    //
+})
---
+++ js/109.itemlist.js
@@ -0,0 +1,63 @@
+const ITEM_LIST = [
+
+    // best_item
+    [
+        { item_no: 0, src: "img/theComma/items/best/1.jpg", name: '빗자루', price: '90,000원', desc: '엄청 비싼 빗자루' }, // 0번 아이템 정보    
+        { item_no: 1, src: "img/theComma/items/best/2.jpg", name: '세면대', price: '190,000원', desc: '세면대 같은 세면대' },
+        { item_no: 2, src: "img/theComma/items/best/3.jpg", name: '원피스', price: '10,000원', desc: '댕댕이 원피스' },
+        { item_no: 3, src: "img/theComma/items/best/4.jpg", name: '가죽 가방', price: '40,000원', desc: '검은가방' },
+        { item_no: 4, src: "img/theComma/items/best/5.jpg", name: '블라우스', price: '50,000원', desc: '검은 블라우스' },
+        { item_no: 0, src: "img/theComma/items/best/1.jpg", name: '빗자루', price: '90,000원', desc: '엄청 비싼 빗자루' }, // 0번 아이템 정보    
+        { item_no: 1, src: "img/theComma/items/best/2.jpg", name: '세면대', price: '190,000원', desc: '세면대 같은 세면대' },
+        { item_no: 2, src: "img/theComma/items/best/3.jpg", name: '원피스', price: '10,000원', desc: '댕댕이 원피스' },
+        { item_no: 3, src: "img/theComma/items/best/4.jpg", name: '가죽 가방', price: '40,000원', desc: '검은가방' },
+        { item_no: 4, src: "img/theComma/items/best/5.jpg", name: '블라우스', price: '50,000원', desc: '검은 블라우스' }
+    ],
+
+    // new
+    [
+        { item_no: 0, src: "img/theComma/items/new/1.jpg", name: '빗자루', price: '90,000원', desc: '엄청 비싼 빗자루' }, // 0번 아이템 정보    
+        { item_no: 1, src: "img/theComma/items/new/2.jpg", name: '세면대', price: '190,000원', desc: '세면대 같은 세면대' },
+        { item_no: 2, src: "img/theComma/items/new/3.jpg", name: '원피스', price: '10,000원', desc: '댕댕이 원피스' },
+        { item_no: 3, src: "img/theComma/items/new/4.jpg", name: '가죽 가방', price: '40,000원', desc: '검은가방' },
+        { item_no: 4, src: "img/theComma/items/new/5.jpg", name: '블라우스', price: '50,000원', desc: '검은 블라우스' },
+        { item_no: 0, src: "img/theComma/items/new/1.jpg", name: '빗자루', price: '90,000원', desc: '엄청 비싼 빗자루' }, // 0번 아이템 정보    
+        { item_no: 1, src: "img/theComma/items/new/2.jpg", name: '세면대', price: '190,000원', desc: '세면대 같은 세면대' },
+        { item_no: 2, src: "img/theComma/items/new/3.jpg", name: '원피스', price: '10,000원', desc: '댕댕이 원피스' },
+        { item_no: 3, src: "img/theComma/items/new/4.jpg", name: '가죽 가방', price: '40,000원', desc: '검은가방' },
+        { item_no: 4, src: "img/theComma/items/new/5.jpg", name: '블라우스', price: '50,000원', desc: '검은 블라우스' },
+        { item_no: 2, src: "img/theComma/items/new/3.jpg", name: '원피스', price: '10,000원', desc: '댕댕이 원피스' },
+        { item_no: 3, src: "img/theComma/items/new/4.jpg", name: '가죽 가방', price: '40,000원', desc: '검은가방' },
+        { item_no: 4, src: "img/theComma/items/new/5.jpg", name: '블라우스', price: '50,000원', desc: '검은 블라우스' }
+    ],
+
+    // top
+    [
+        { item_no: 2, src: "img/theComma/items/best/3.jpg", name: '원피스', price: '10,000원', desc: '댕댕이 원피스' },
+        { item_no: 3, src: "img/theComma/items/best/4.jpg", name: '가죽 가방', price: '40,000원', desc: '검은가방' },
+        { item_no: 4, src: "img/theComma/items/best/5.jpg", name: '블라우스', price: '50,000원', desc: '검은 블라우스' },
+        { item_no: 0, src: "img/theComma/items/best/1.jpg", name: '빗자루', price: '90,000원', desc: '엄청 비싼 빗자루' }, // 0번 아이템 정보    
+        { item_no: 0, src: "img/theComma/items/best/1.jpg", name: '빗자루', price: '90,000원', desc: '엄청 비싼 빗자루' }, // 0번 아이템 정보    
+        { item_no: 1, src: "img/theComma/items/best/2.jpg", name: '세면대', price: '190,000원', desc: '세면대 같은 세면대' },
+
+        { item_no: 1, src: "img/theComma/items/best/2.jpg", name: '세면대', price: '190,000원', desc: '세면대 같은 세면대' },
+        { item_no: 2, src: "img/theComma/items/best/3.jpg", name: '원피스', price: '10,000원', desc: '댕댕이 원피스' },
+        { item_no: 3, src: "img/theComma/items/best/4.jpg", name: '가죽 가방', price: '40,000원', desc: '검은가방' },
+        { item_no: 4, src: "img/theComma/items/best/5.jpg", name: '블라우스', price: '50,000원', desc: '검은 블라우스' }
+    ],
+
+    
+    // bottom
+    [
+        { item_no: 4, src: "img/theComma/items/new/5.jpg", name: '블라우스', price: '50,000원', desc: '검은 블라우스' },
+        { item_no: 0, src: "img/theComma/items/new/1.jpg", name: '빗자루', price: '90,000원', desc: '엄청 비싼 빗자루' }, // 0번 아이템 정보    
+        { item_no: 1, src: "img/theComma/items/new/2.jpg", name: '세면대', price: '190,000원', desc: '세면대 같은 세면대' },
+        { item_no: 2, src: "img/theComma/items/new/3.jpg", name: '원피스', price: '10,000원', desc: '댕댕이 원피스' },
+        { item_no: 3, src: "img/theComma/items/new/4.jpg", name: '가죽 가방', price: '40,000원', desc: '검은가방' },{ item_no: 0, src: "img/theComma/items/new/1.jpg", name: '빗자루', price: '90,000원', desc: '엄청 비싼 빗자루' }, // 0번 아이템 정보    
+        { item_no: 1, src: "img/theComma/items/new/2.jpg", name: '세면대', price: '190,000원', desc: '세면대 같은 세면대' },
+        { item_no: 2, src: "img/theComma/items/new/3.jpg", name: '원피스', price: '10,000원', desc: '댕댕이 원피스' },
+        { item_no: 3, src: "img/theComma/items/new/4.jpg", name: '가죽 가방', price: '40,000원', desc: '검은가방' },
+        { item_no: 4, src: "img/theComma/items/new/5.jpg", name: '블라우스', price: '50,000원', desc: '검은 블라우스' }
+    ],
+
+];    
